2f0fd3527a854090de5af6efc0aa7f6f4a86acdb
María Camila Lara <mclarac91@gmail.com>
2021-02-19 20:58:11 -0500

    background color

--- utils.R
+++ utils.R
@@ -1,20 +1,19 @@
 my_theme <- theme_gray() + 
-    theme(
-        panel.background = element_rect(fill = "#e5e5e5"),
-        plot.background = element_rect(fill = "#e5e5e5"),
-        legend.box.background = element_rect(fill = "#e5e5e5", color = "#e5e5e5"),
-        legend.background = element_rect(fill = "#e5e5e5", color = "#e5e5e5"),
-        legend.text = element_text(color = "#2c241e"),
-        legend.title = element_text(color = "#2c241e"),
-        legend.key = element_rect(fill = "#e5e5e5", color = "#e5e5e5"),
-        axis.text = element_text(color = "#2c241e"),
-        axis.title = element_text(color = "#2c241e"),
-        plot.title = element_text(color = "#2c241e"),
-        plot.subtitle = element_text(color = "#2c241e", size = 9),
-        axis.line.y = element_blank(),
-        axis.line.x = element_line(color = "#2c241e"),
-        panel.grid.major.y = element_line(color = "#2c241e", linetype = "dashed", size = .05),
-        panel.grid = element_blank(),
-        panel.border = element_blank(),
-        axis.ticks = element_blank()
-    )
+  theme(
+    legend.text = element_text(color = "#2c241e"),
+    legend.title = element_text(color = "#2c241e"),
+    axis.text = element_text(color = "#2c241e"),
+    axis.title = element_text(color = "#2c241e"),
+    plot.title = element_text(color = "#2c241e"),
+    plot.subtitle = element_text(color = "#2c241e", size = 9),
+    axis.line.y = element_blank(),
+    axis.line.x = element_line(color = "#2c241e"),
+    panel.grid.major.y = element_line(
+      color = "#2c241e", 
+      linetype = "dashed", 
+      size = .05
+    ),
+    panel.grid = element_blank(),
+    panel.border = element_blank(),
+    axis.ticks = element_blank()
+  )
